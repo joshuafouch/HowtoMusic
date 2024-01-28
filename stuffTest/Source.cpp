7a531b002594fde29826c09c7ae3f3550b1f964e
joshua-angelo <jfouch001@student.franciscan.edu>
2024-01-27 21:49:08 -0500

    Add project files.

---
+++ stuffTest/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+#pragma comment(lib, "winmm.lib")
+
+int main()
+{
+	cout << "Playing music \n";
+
+	PlaySound(TEXT("goffybeat.wav"), NULL, SND_FILENAME | SND_ASYNC);
+
+	cout << "How is your day?" << endl;
+	int choice;
+	cin >> choice;
+
+	cout << "heh...";
+
+	for (int i = 0; i <= 100; i++)
+	{
+		cout << "bruh moment..." << endl;
+	}
+
+	cout << "ok bye;";
+
+	return 0;
+}
